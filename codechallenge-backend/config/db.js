64cc072a43ff0811aff963ee5434fab35f2f075b
Hans Diary <diaryraoelison508@gmail.com>
2026-06-29 15:13:27 +0300

    Refactor database connection to use environment variable

--- codechallenge-backend/config/db.js
+++ codechallenge-backend/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "codechallenge_0b70_user",
-    host: "localhost",
-    database: "codechallenge",
-    password: "wcuQjzlEWx79WK8OV2ka5nbLIo5hAr6Y",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
