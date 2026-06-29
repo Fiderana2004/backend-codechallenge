1e756bd6e5f6d3232324bed7497abdd2f0a8bc2c
Hans Diary <diaryraoelison508@gmail.com>
2026-06-29 15:35:35 +0300

    Serve admin.html on root route

--- codechallenge-backend/server.js
+++ codechallenge-backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
@@ -18,6 +19,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin.html"));
+});
 
 app.use(
   "/uploads",
